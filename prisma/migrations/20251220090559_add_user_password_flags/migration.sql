df39535c806af147acbc20ffd207b4c6dcaecc6f
BreederHQ Admin <admin@breederhq.com>
2025-12-20 09:06:01 -0600

    Add user password flags columns

---
+++ prisma/migrations/20251220090559_add_user_password_flags/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "lastPasswordChangeAt" TIMESTAMP(3),
+ADD COLUMN     "mustChangePassword" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "passwordSetAt" TIMESTAMP(3);
+
